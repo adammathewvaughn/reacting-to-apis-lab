a9b8573a1c1251ef06774d8e12337e331335ba0c
Adam Vaughn <adammathewvaughn>
2021-07-01 21:40:22 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+const App = () => {
+  return (
+    <>
+      <div class="row justify-content-center p-0">
+        <div class="container"></div>
+        <div class="jumbotron jumbotron-fluid rounded m-5 pt-5 text-white bg-primary">
+          <div class="container">
+            <h1 class="display-5">Studio Ghibli</h1>
+            <h2 class="display-5 pb-5">Movies</h2>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default App;
