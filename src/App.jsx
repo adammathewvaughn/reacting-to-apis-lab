a63ae02e201eefd96255f21b768b6d4b4c597ec0
Adam Vaughn <adammathewvaughn>
2021-07-02 18:27:45 -0500

    mostly functional

--- src/App.jsx
+++ src/App.jsx
@@ -30,20 +30,12 @@ const App = () => {
             <p className="lead pt-2 mt-2"> (株式会社スタジオジブリ</p>
           </div>
         </div>
-        <button type="button" className="btn btn-primary btn-lg px-4 gap-3">Primary button</button>
-        <button type="button" className="btn btn-outline-secondary btn-lg px-4">Secondary</button>
+      </div>
+       <div className="d-flex justify-content-center"> 
+       <button type="button" className="btn btn-primary">List Movies</button>
+        <button type="button" className="btn btn-outline-secondary">List Characters</button>
       </div>
 
-      <div className="px-4 py-5 my-5 text-center">
-
-    <h1 className="display-5 fw-bold">Centered hero</h1>
-    <div className="col-lg-6 mx-auto">
-      <p className="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
-      <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">﷯
-      </div>
-      </div>
-      </div>
-     {/* adding multiple props */}
           {films.map(film=><Films key={film.id} film={film} />)} 
           {people.map(peeps=><People key={peeps.id} peeps={peeps} />)} 
 
--- src/components/People.jsx
+++ src/components/People.jsx
@@ -3,18 +3,17 @@ import React from "react";
 const People = ({ peeps }) => {
   return (
     <>
-      <div className="card">
+      <div className="card d-flex row px-4 py-5 my-5 text-center">
         <div className="card-body">
-          <h5 className="card-title">Card title</h5>
+          <h5 className="card-title">{peeps.name}</h5>
           <p className="card-text">
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
           </p>
         </div>
-        <ul className="list-group list-group-flush">
-          <li className="list-group-item">{peeps.id}</li>
-          <li className="list-group-item">A second item</li>
-          <li className="list-group-item">A third item</li>
+        <ul className="list-group ">
+          <li className="list-group-item">{peeps.age}</li>
+          <li className="list-group-item">{peeps.hair_color}</li>
         </ul>
         <div className="card-body">
           {/* <a href="#" className="card-link">Card link</a>
